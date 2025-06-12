585dc9f38bd591860d7edbb84e8b62b6965810a6
Frengk Iskandar <frankierockabilly@gmail.com>
2025-06-12 09:23:59 +0700

    update

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -8,80 +8,134 @@ const Header = ({ scrollToAbout, scrollToHome, scrollToPortfolio, scrollToContac
 
     const [scroll, setScroll] = useState(false)
 
+    const refresh = () => {
+       window.location.reload();
+    };
+
     const handleScroll = () => {
-        console.log(window.scrollY);
+       console.log(window.scrollY);
 
-        if (window.scrollY > 50) {
-            setScroll(true)
-        } else {
-            setScroll(false)
-        }
-    }
+       if (window.scrollY > 50) {
+          setScroll(true);
+       } else {
+          setScroll(false);
+       }
+    };
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
+       window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
+       return () => {
+          window.removeEventListener("scroll", handleScroll);
+       };
+    }, []);
 
     return (
-        <>
-            <header className={`flex justify-between items-center px-[10%] py-10 flex-grow-0 h-20 w-full fixed top-0 z-30 right-0 left-0  ${scroll ? 'shadow-md backdrop-blur-3xl bg-opacity-60' : ''}`}>
-                <div className='flex justify-center items-center gap-20'>
-                    <p className='font-bold text-xl text-primary'>FRENGKI <span className='text-oren'>ISKANDAR</span></p>
-                </div>
+       <>
+          <header
+             className={`flex justify-between items-center px-[10%] py-10 flex-grow-0 h-20 w-full fixed top-0 z-30 right-0 left-0  ${
+                scroll ? "shadow-md backdrop-blur-3xl bg-opacity-60" : ""
+             }`}
+          >
+             <div
+                className="flex justify-center items-center gap-20 cursor-pointer"
+                onClick={refresh}
+             >
+                <p className="font-bold text-xl text-primary">
+                   FRENGKI <span className="text-oren">ISKANDAR</span>
+                </p>
+             </div>
 
-                <div>
-                    <div className='lg:hidden '>
-                        <div className="drawer">
-                            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-                            <div className="drawer-content">
-                                {/* Page content here */}
-                                <label htmlFor="my-drawer" className=" drawer-button"><RxHamburgerMenu /></label>
+             <div>
+                <div className="lg:hidden ">
+                   <div className="drawer">
+                      <input
+                         id="my-drawer"
+                         type="checkbox"
+                         className="drawer-toggle"
+                      />
+                      <div className="drawer-content">
+                         {/* Page content here */}
+                         <label htmlFor="my-drawer" className=" drawer-button">
+                            <RxHamburgerMenu />
+                         </label>
+                      </div>
+                      <div className="drawer-side ">
+                         <label
+                            htmlFor="my-drawer"
+                            aria-label="close sidebar"
+                            className="drawer-overlay"
+                         ></label>
+                         <div className="menu bg-white min-h-full w-72 p-4 gap-10 text-base font-light py-10 shadow-xl text-black">
+                            <div
+                               className="flex justify-start items-center gap-5 hover:text-oren cursor-pointer"
+                               onClick={() => scrollToHome()}
+                            >
+                               <FaHome />
+                               <p>Home</p>
                             </div>
-                            <div className="drawer-side ">
-                                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                                <div className="menu bg-white min-h-full w-72 p-4 gap-10 text-base font-light py-10 shadow-xl text-black">
-                                    <div className='flex justify-start items-center gap-5 hover:text-oren cursor-pointer' onClick={() => scrollToHome()}>
-                                        <FaHome />
-                                        <p>Home</p>
-                                    </div>
-                                    <div className='flex justify-start items-center gap-5 hover:text-oren cursor-pointer' onClick={() => scrollToAbout()}>
-                                        <FaClipboardList />
-                                        <p>About</p>
-                                    </div>
-                                    <div className='flex justify-start items-center gap-5 hover:text-oren cursor-pointer' onClick={() => scrollToPortfolio()}>
-                                        <LuLayoutList />
-                                        <p>Portfolio</p>
-                                    </div>
-                                    <div className='flex justify-start items-center gap-5 hover:text-oren cursor-pointer' onClick={() => scrollToContact()}>
-                                        <FaPhone />
-                                        <p>Contact</p>
-                                    </div>
-                                    <div className='flex justify-start items-center gap-5'>
-                                        <FaFacebook size={20} />
-                                        <FaInstagram size={20} />
-                                        <FaWhatsapp size={20} />
-                                    </div>
-                                </div>
+                            <div
+                               className="flex justify-start items-center gap-5 hover:text-oren cursor-pointer"
+                               onClick={() => scrollToAbout()}
+                            >
+                               <FaClipboardList />
+                               <p>About</p>
                             </div>
-                        </div>
-                    </div>
-                    <div className='hidden lg:flex justify-center items-center gap-20 text-xl text-zinc-500'>
-                        <button className='font-light py-2 hover:text-primary' onClick={() => scrollToHome()}>Home</button>
-                        <button className='font-light py-2 hover:text-primary' onClick={() => scrollToAbout()}>About</button>
-                        <button className='font-light py-2 hover:text-primary' onClick={() => scrollToPortfolio()}>Portolio</button>
-                        <button className='font-light py-2 hover:text-primary' onClick={() => scrollToContact()}>Contact</button>
-                        {/* <div className='hidden md:block text-xl'>
+                            <div
+                               className="flex justify-start items-center gap-5 hover:text-oren cursor-pointer"
+                               onClick={() => scrollToPortfolio()}
+                            >
+                               <LuLayoutList />
+                               <p>Portfolio</p>
+                            </div>
+                            <div
+                               className="flex justify-start items-center gap-5 hover:text-oren cursor-pointer"
+                               onClick={() => scrollToContact()}
+                            >
+                               <FaPhone />
+                               <p>Contact</p>
+                            </div>
+                            <div className="flex justify-start items-center gap-5">
+                               <FaFacebook size={20} />
+                               <FaInstagram size={20} />
+                               <FaWhatsapp size={20} />
+                            </div>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+                <div className="hidden lg:flex justify-center items-center gap-20 text-xl text-zinc-500">
+                   <button
+                      className="font-light py-2 hover:text-primary"
+                      onClick={() => scrollToHome()}
+                   >
+                      Home
+                   </button>
+                   <button
+                      className="font-light py-2 hover:text-primary"
+                      onClick={() => scrollToAbout()}
+                   >
+                      About
+                   </button>
+                   <button
+                      className="font-light py-2 hover:text-primary"
+                      onClick={() => scrollToPortfolio()}
+                   >
+                      Portolio
+                   </button>
+                   <button
+                      className="font-light py-2 hover:text-primary"
+                      onClick={() => scrollToContact()}
+                   >
+                      Contact
+                   </button>
+                   {/* <div className='hidden md:block text-xl'>
                             <button className='bg-secondary px-7 py-3 rounded-lg text-black'>Contact Me</button>
                         </div> */}
-                    </div>
-
                 </div>
-            </header >
-        </>
-    )
+             </div>
+          </header>
+       </>
+    );
 }
 
 export default Header
--- src/Component/Testimonial.jsx
+++ src/Component/Testimonial.jsx
@@ -8,49 +8,59 @@ import testimonial from "../Assets/Json/testimonial.json";
 
 const Testimonial = () => {
     return (
-        <div className="bg-gray-100 py-20 ">
-            <div className="flex flex-col justify-center items-center gap-6 pt-12">
-                <h1 className="text-center text-2xl lg:text-3xl font-bold">Testimonial</h1>
-                <Line />
-            </div>
+       <div className="bg-gray-100 py-20 ">
+          <div className="flex flex-col justify-center items-center gap-6 pt-12">
+             <h1 className="text-center text-2xl lg:text-3xl font-bold">
+                Testimonial
+             </h1>
+             <Line />
+          </div>
 
-            {/* Infinite Carousel */}
-            <div className="py-12">
-                <Swiper
-                    loop={true} // Infinite scroll
-                    autoplay={{
-                        delay: 0, // Tidak ada delay
-                        disableOnInteraction: false, // Tetap berjalan meski ada interaksi
-                    }}
-                    speed={5000} // Kecepatan scroll (ms)
-                    modules={[Autoplay]} // Menggunakan modul Autoplay
-                    className="w-full"
-                    breakpoints={{
-                        320: { slidesPerView: 1, spaceBetween: 5 }, // Mobile
-                        640: { slidesPerView: 2, spaceBetween: 5 }, // Tablet
-                        1024: { slidesPerView: 3, spaceBetween: 15 }, // Laptop
-                        1280: { slidesPerView: 4, spaceBetween: 20 }, // Desktop
-                    }}
-                >
-                    {testimonial.map((value, index) => (
-                        <SwiperSlide key={index} className="flex justify-center">
-                            <div className="bg-white rounded-lg px-4 md:px-12 py-5 shadow-md w-[20rem] md:w-[27rem] h-60 overflow-hidden flex flex-col justify-start items-start gap-2">
-                                <div className="flex justify-center items-center gap-4">
-                                    <div className="w-20 h-20 rounded-full border flex justify-center items-center">
-                                        <img src={value.profilImage} alt="profil" className="w-full h-full object-cover rounded-full" />
-                                    </div>
-                                    <div>
-                                        <p className="font-semibold ">{value.name}</p>
-                                        <p className="text-sm text-gray-400">{value.bisnis}</p>
-                                    </div>
-                                </div>
-                                <p className="text-gray-500 font-light truncate w-full text-wrap text-sm lg:text-base">{value.comment}</p>
+          {/* Infinite Carousel */}
+          <div className="py-12">
+             <Swiper
+                loop={true} // Infinite scroll
+                autoplay={{
+                   delay: 0, // Tidak ada delay
+                   disableOnInteraction: false, // Tetap berjalan meski ada interaksi
+                }}
+                speed={5000} // Kecepatan scroll (ms)
+                modules={[Autoplay]} // Menggunakan modul Autoplay
+                className="w-full"
+                breakpoints={{
+                   320: { slidesPerView: 1, spaceBetween: 5 }, // Mobile
+                   640: { slidesPerView: 2, spaceBetween: 5 }, // Tablet
+                   1024: { slidesPerView: 3, spaceBetween: 15 }, // Laptop
+                   1280: { slidesPerView: 4, spaceBetween: 20 }, // Desktop
+                }}
+             >
+                {testimonial.map((value, index) => (
+                   <SwiperSlide key={index} className="flex justify-center">
+                      <div className="bg-white rounded-lg px-4 md:px-12 py-5 shadow-md w-[20rem] md:w-[27rem] h-60 overflow-hidden flex flex-col justify-start items-start gap-2">
+                         <div className="flex justify-center items-center gap-4">
+                            <div className="w-20 h-20 rounded-full border flex justify-center items-center">
+                               <img
+                                  src={value.profilImage}
+                                  alt="profil"
+                                  className="w-full h-full object-cover rounded-full border border-red-500"
+                               />
                             </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-        </div>
+                            <div>
+                               <p className="font-semibold ">{value.name}</p>
+                               <p className="text-sm text-gray-400">
+                                  {value.bisnis} - {value.location}
+                               </p>
+                            </div>
+                         </div>
+                         <p className="text-gray-500 font-light truncate w-full text-wrap text-sm lg:text-base">
+                            {value.comment}
+                         </p>
+                      </div>
+                   </SwiperSlide>
+                ))}
+             </Swiper>
+          </div>
+       </div>
     );
 };
 
